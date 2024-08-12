13980a9db69127c866ebfe428a8323e53d715441
zucchi100 <alicasa007@gmail.com>
2024-08-12 15:35:59 -0300

    burguer

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+dialect: "postgres",
+host:"localhost",
+username: "postgres",
+password: "postgres",
+database: "devburguer",
+define: {
+timestamps: true,
+underscored: true,
+underscoredAll: true,
+
+
+},
+};
